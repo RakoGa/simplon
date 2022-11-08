e4cd0616b59b40afcfbd538a5f9c418814b676f3
RakoGa <gaetan.rakotomalala38@gmail.com>
2022-11-08 22:49:01 +0100

    08/11

--- javascript/td1/exercice12.js
+++ javascript/td1/exercice12.js
@@ -1,10 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+
 // create table
 
 // header rows/columns
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i <= 10; i++) {
     // create column
-    for (let j = 1; i <= 10; j++) {
+    // if 0 => header
+    for (let j = 0; i <= 10; j++) {
         // create and fill cell
+        // if 0 => header
     }
 }
