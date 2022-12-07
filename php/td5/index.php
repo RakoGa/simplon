d10d9b7517ef630c0785417acbbc5959fec361f6
RakoGa <gaetan.rakotomalala38@gmail.com>
2022-12-07 16:43:24 +0100

    07/12 afternoon

---
+++ php/td5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Employés</h1>
+
+    <h2>Accéder à la section de gestion des employés:</h2>
+    <ul>
+        <li>Ajouter un nouvel employé, modifier, supprimer</li>
+        <li>Afficher un état</li>
+        <li>etc.</li>
+    </ul>
+
+    <p>Cliquer sur le bouton ci-dessous.</p>
+    
+</body>
+</html>
